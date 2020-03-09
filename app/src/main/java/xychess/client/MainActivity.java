6cf1caffb4a26104cb9548728bf555e43b5cb0b7
Tyler Wheaton <xyzentw@gmail.com>
2020-03-09 11:09:34 -0500

    Added turn indicator TextView

--- app/src/main/java/xychess/client/MainActivity.java
+++ app/src/main/java/xychess/client/MainActivity.java
@@ -2,23 +2,28 @@ package xychess.client;
 
         import androidx.appcompat.app.AppCompatActivity;
         import android.widget.ImageView;
+        import android.widget.TextView;
         import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String selected = "";
+    private char turn = 'w';
+    private String[][] board;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initBoard(findViewById(0));
+        // initialize board
+        initBoard();
     }
 
-    private String selected = "";
-    private char turn = 'w';
-    private String[][]board;
-
-    private static final java.util.HashMap<String, Integer> tokenImg = new java.util.HashMap<>();
+    private static final java.util.HashMap<String, Integer>
+            // mapping from string to int
+            tokenImg = new java.util.HashMap<>();
     static {
+        // mapping from piece token to drawable image resource
         tokenImg.put("wr", R.drawable.white_rook);
         tokenImg.put("wn", R.drawable.white_knight);
         tokenImg.put("wb", R.drawable.white_bishop);
@@ -34,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private static final int[][] cells = {
+            // board UI components / ImageViews
+            // *should* appear upside-down here
             { R.id.cell_a0, R.id.cell_b0, R.id.cell_c0, R.id.cell_d0, R.id.cell_e0, R.id.cell_f0, R.id.cell_g0, R.id.cell_h0 },
             { R.id.cell_a1, R.id.cell_b1, R.id.cell_c1, R.id.cell_d1, R.id.cell_e1, R.id.cell_f1, R.id.cell_g1, R.id.cell_h1 },
             { R.id.cell_a2, R.id.cell_b2, R.id.cell_c2, R.id.cell_d2, R.id.cell_e2, R.id.cell_f2, R.id.cell_g2, R.id.cell_h2 },
@@ -43,6 +50,70 @@ public class MainActivity extends AppCompatActivity {
             { R.id.cell_a6, R.id.cell_b6, R.id.cell_c6, R.id.cell_d6, R.id.cell_e6, R.id.cell_f6, R.id.cell_g6, R.id.cell_h6 },
             { R.id.cell_a7, R.id.cell_b7, R.id.cell_c7, R.id.cell_d7, R.id.cell_e7, R.id.cell_f7, R.id.cell_g7, R.id.cell_h7 }
     };
+
+    private void initBoard() {
+        // no View is attached to this call
+        // --> 0 is just a garbage value
+        initBoard(findViewById(0));
+    }
+
+    public void initBoard(android.view.View view) {
+        board = new String[][]{
+                // initial board piece tokens
+                // *should* appear upside-down here
+                {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"},
+                {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
+                {"", "", "", "", "", "", "", ""},
+                {"", "", "", "", "", "", "", ""},
+                {"", "", "", "", "", "", "", ""},
+                {"", "", "", "", "", "", "", ""},
+                {"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"},
+                {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"}
+        };
+        // refresh UI
+        refreshBoardView();
+        refreshTurnView("White to Move");
+        // reinitialize data values
+        selected = "";
+        turn = 'w';
+    }
+
+    private void changeTurns() {
+        // toggle turn char and refresh turn view
+        if (turn == 'w') {
+            turn = 'b';
+            refreshTurnView("Black to Move");
+        }
+        else {
+            turn = 'w';
+            refreshTurnView("White to Move");
+        }
+    }
+
+    private void refreshTurnView(String text) {
+        ((TextView) findViewById(R.id.turn_indicator))
+                .setText(text);
+    }
+
+    private void refreshBoardView() {
+        String piece;
+        for (int rank = 0; rank < 8; rank++) {
+            for (int file = 0; file < 8; file++) {
+                piece = board[rank][file];
+                if (tokenImg.containsKey(piece)) {
+                    refreshCellView(rank, file, tokenImg.get(piece));
+                }
+                else {
+                    refreshCellView(rank, file, 0);
+                }
+            }
+        }
+    }
+
+    private void refreshCellView(int rank, int file, int drawable) {
+        ((ImageView) findViewById(cells[rank][file]))
+                .setImageResource(drawable);
+    }
 
     public void selectCell(android.view.View view) {
         int view_id = view.getId();
@@ -59,61 +130,23 @@ public class MainActivity extends AppCompatActivity {
         if (rank == 8 || file == 8) {
             return;
         }
+        String token = board[rank][file];
         if (selected == "") {
-            String token = board[rank][file];
             if (token == "" || token.charAt(0) != turn) {
                 return;
             }
             ((ImageView) findViewById(view_id)).setImageResource(0);
-            selected = board[rank][file];
+            selected = token;
             board[rank][file] = "";
         }
         else if (tokenImg.containsKey(selected)) {
+            if (!checkMove(rank, file, token)) {
+                return;
+            }
             ((ImageView) findViewById(view_id)).setImageResource(tokenImg.get(selected));
             board[rank][file] = selected;
             selected = "";
             changeTurns();
-        }
-    }
-
-    private void changeTurns() {
-        if (turn == 'w') {
-            turn = 'b';
-        }
-        else {
-            turn = 'w';
-        }
-    }
-
-    public void initBoard(android.view.View view) {
-        board = new String[][]{
-                // initial board: *should* appear upside-down here
-                {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"},
-                {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
-                {"", "", "", "", "", "", "", ""},
-                {"", "", "", "", "", "", "", ""},
-                {"", "", "", "", "", "", "", ""},
-                {"", "", "", "", "", "", "", ""},
-                {"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"},
-                {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"}
-        };
-        refreshView();
-        selected = "";
-        turn = 'w';
-    }
-
-    private void refreshView() {
-        String piece;
-        for (int rank = 0; rank < 8; rank++) {
-            for (int file = 0; file < 8; file++) {
-                piece = board[rank][file];
-                if (tokenImg.containsKey(piece)) {
-                    ((ImageView) findViewById(cells[rank][file])).setImageResource(tokenImg.get(piece));
-                }
-                else {
-                    ((ImageView) findViewById(cells[rank][file])).setImageResource(0);
-                }
-            }
         }
     }
 
