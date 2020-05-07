f57c1350953d8e630f883c01f99ea99f960e7858
Tyler Wheaton <xyzentw@gmail.com>
2020-05-07 18:47:05 -0500

    Added castling logic

--- app/src/main/java/xychess/client/BoardState.java
+++ app/src/main/java/xychess/client/BoardState.java
@@ -18,6 +18,12 @@ public class BoardState {
     private int bep_head_rank, bep_head_file,
             bep_tail_rank, bep_tail_file;
 
+    // Castling flags
+    private boolean wk_moved, wra_moved, wrh_moved,
+            wa_cstl_pending, wh_cstl_pending,
+            bk_moved, bra_moved, brh_moved,
+            ba_cstl_pending, bh_cstl_pending;
+
     public BoardState() {
         newGame();
     }
@@ -42,6 +48,11 @@ public class BoardState {
                 = bep_head_rank = bep_head_file
                 = bep_tail_rank = bep_tail_file
                 = 8;
+        wk_moved = wra_moved = wrh_moved
+                = wa_cstl_pending = wh_cstl_pending
+                = bk_moved = bra_moved = brh_moved
+                = ba_cstl_pending = bh_cstl_pending
+                = false;
     }
 
     public String getToken(int rank, int file) {
@@ -62,9 +73,10 @@ public class BoardState {
             return false;
         BoardState new_state = commitMove(old_rank, old_file, new_rank, new_file);
         boolean legal_state = !new_state.inCheck(whose_turn);
-        if (legal_state) {
+        if (legal_state)
             setState(new_state);
-        }
+        else
+            resetCastlePending();
         return legal_state;
     }
 
@@ -95,20 +107,67 @@ public class BoardState {
         }
     }
 
+    private void updateCastleFlags(String token, int rank, int file) {
+        if (token.equals("wk")) {
+            wk_moved = true;
+        }
+        else if (token.equals("wr")) {
+            if (file == 0 && rank == 0)
+                wra_moved = true;
+            else if (file == 7 && rank == 0)
+                wrh_moved = true;
+        } else if (token.equals("bk")) {
+            bk_moved = true;
+        }
+        else if (token.equals("br")) {
+            if (file == 0 && rank == 7)
+                bra_moved = true;
+            else if (file == 7 && rank == 7)
+                brh_moved = true;
+        }
+    }
+
+    private void resetCastlePending() {
+        if (whose_turn == 'w')
+            wa_cstl_pending = wh_cstl_pending = false;
+        else
+            ba_cstl_pending = bh_cstl_pending = false;
+    }
+
+    private void resolveCastling() {
+        if (wa_cstl_pending) {
+            board[0][0] = "";
+            board[0][3] = "wr";
+            wa_cstl_pending = false;
+        } else if (wh_cstl_pending) {
+            board[0][7] = "";
+            board[0][5] = "wr";
+            wh_cstl_pending = false;
+        } else if (ba_cstl_pending) {
+            board[7][0] = "";
+            board[7][3] = "br";
+            ba_cstl_pending = false;
+        } else if (bh_cstl_pending) {
+            board[7][7] = "";
+            board[7][5] = "br";
+            bh_cstl_pending = false;
+        }
+    }
+
     private BoardState commitMove(int old_rank, int old_file, int new_rank, int new_file) {
         BoardState new_state = new BoardState();
         new_state.setState(this);
-        new_state.board[new_rank][new_file] = new_state.board[old_rank][old_file];
+        String token = new_state.board[old_rank][old_file];
         new_state.board[old_rank][old_file] = "";
+        new_state.board[new_rank][new_file] = token;
+        new_state.resolveCastling();
+        new_state.updateCastleFlags(token, old_rank, old_file);
         new_state.resolveEnPassant();
         new_state.changeTurns();
         return new_state;
     }
 
-    private boolean inCheck(char team) {
-        int[] rank_file = getKingRankFile(team);
-        int king_rank = rank_file[0],
-                king_file = rank_file[1];
+    private boolean underAttack(char team, int rank, int file) {
         String token;
         char color;
         char opponent = opponent(team);
@@ -118,12 +177,19 @@ public class BoardState {
                 if (token.equals(""))
                     continue;
                 color = token.charAt(0);
-                if (color == opponent && checkMovement(r, f, king_rank, king_file, false)) {
+                if (color == opponent && checkMovement(r, f, rank, file, false)) {
                     return true;
                 }
             }
         }
         return false;
+    }
+
+    private boolean inCheck(char team) {
+        int[] rank_file = getKingRankFile(team);
+        int king_rank = rank_file[0],
+                king_file = rank_file[1];
+        return underAttack(team, king_rank, king_file);
     }
 
     private int[] getKingRankFile(char team) {
@@ -157,6 +223,16 @@ public class BoardState {
         bep_head_file = other.bep_head_file;
         bep_tail_rank = other.bep_tail_rank;
         bep_tail_file = other.bep_tail_file;
+        wk_moved = other.wk_moved;
+        wra_moved = other.wra_moved;
+        wrh_moved = other.wrh_moved;
+        wa_cstl_pending = other.wa_cstl_pending;
+        wh_cstl_pending = other.wh_cstl_pending;
+        bk_moved = other.bk_moved;
+        bra_moved = other.bra_moved;
+        brh_moved = other.brh_moved;
+        ba_cstl_pending = other.ba_cstl_pending;
+        bh_cstl_pending = other.bh_cstl_pending;
     }
 
     private boolean checkMovement(int old_rank, int old_file, int new_rank, int new_file, boolean flagging) {
@@ -291,9 +367,69 @@ public class BoardState {
         int rank_diff = Math.abs(new_rank - old_rank),
                 file_diff = Math.abs(new_file - old_file);
         // Check for 1x0, 0x1, or 1x1 traversal
-        return (rank_diff == 1 && file_diff == 0)
+        if ((rank_diff == 1 && file_diff == 0)
                 || (rank_diff == 0 && file_diff == 1)
-                || (rank_diff == 1 && file_diff == 1);
+                || (rank_diff == 1 && file_diff == 1))
+            return true;
+        // Check for castling moves
+        return checkCastling(old_rank, old_file, new_rank, new_file);
+    }
+
+    private boolean checkCastling(int old_rank, int old_file, int new_rank, int new_file) {
+        // Filter moves from wrong file
+        if (old_file != 4)
+            return false;
+        // Filter moves to wrong file
+        if (new_file != 2 && new_file != 6)
+            return false;;
+        // Filter by team, rank, flag, and file
+        if (whose_turn == 'w') {
+            if (wk_moved || old_rank != 0 || new_rank != 0)
+                return false;
+            if (new_file == 2) {
+                if (wra_moved)
+                    return false;
+                wa_cstl_pending = true;
+            }
+            if (new_file == 6) {
+                if (wrh_moved)
+                    return false;
+                wh_cstl_pending = true;
+            }
+        } else {
+            if (bk_moved || old_rank != 7 || new_rank != 7)
+                return false;
+            if (new_file == 2) {
+                if (bra_moved)
+                    return false;
+                ba_cstl_pending = true;
+            }
+            if (new_file == 6) {
+                if (brh_moved)
+                    return false;
+                bh_cstl_pending = true;
+            }
+        }
+        // Get file step
+        int file_diff = new_file - old_file;
+        int file_step = file_diff == 0 ? 0 : file_diff < 0 ? -1 : 1;
+        // Filter unsafe Kings
+        int r = old_rank, f = old_file;
+        if (underAttack(whose_turn, r, f))
+            return false;
+        f += file_step;
+        // Check that all intermediate squares are empty and safe
+        String token;
+        while (f != 0 && f != 7) {
+            token = board[r][f];
+            if (!token.equals("") || underAttack(whose_turn, r, f))
+                return false;
+            f += file_step;
+        }
+        // Check that rook is not under attack
+        if (underAttack(whose_turn, r, f))
+            return false;
+        return true;
     }
 
     private boolean clearDiagonal(int old_rank, int old_file, int new_rank, int new_file) {
