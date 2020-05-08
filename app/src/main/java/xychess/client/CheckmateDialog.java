84c5c861de2f5845790ad608977a1372980a67b2
Tyler Wheaton <xyzentw@gmail.com>
2020-05-08 11:54:16 -0500

    Added CheckmateDialog class

--- app/src/main/java/xychess/client/CheckmateDialog.java
+++ app/src/main/java/xychess/client/CheckmateDialog.java
@@ -7,22 +7,19 @@ import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
-public class ConfirmNewGameDialog extends AppCompatDialogFragment {
-    private MainActivity master;
-    ConfirmNewGameDialog(MainActivity root) {
-        master = root;
+public class CheckmateDialog extends AppCompatDialogFragment {
+
+    String message;
+
+    public CheckmateDialog(String message) {
+        this.message = message;
     }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Start a New Game?");
-        builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which){
-                master.newGame();
-                dialog.dismiss();
-            }
-        }).setPositiveButton("No", new DialogInterface.OnClickListener() {
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
